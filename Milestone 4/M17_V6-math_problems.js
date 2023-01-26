c1abf8ce06687142093902575b357f8c6d72b4b5
akil-tajwar <akiltajwar@gmail.com>
2023-01-26 16:47:10 +0600

    ph notes

---
+++ Milestone 4/M17_V6-math_problems.js
@@ -0,0 +1,9 @@
+var potato = 25;
+var onion = 50;
+var totalPrice = potato + onion;
+console.log(totalPrice);
+
+var potato = 0.1;
+var onion = 0.2;
+var total = potato + onion;
+console.log(total.toFixed(1));
