6812f749eec24c43fad267e2ff62217e4af6c4da
akil-tajwar <akiltajwar@gmail.com>
2023-01-27 12:04:32 +0600

    ph notes

---
+++ Milestone 4/M18_V3-element_and_index.js
@@ -0,0 +1,13 @@
+var numbers = [54, 23, 64, 74];
+
+// showing output of array
+console.log(numbers);
+
+// showing the length of a particular array
+console.log(numbers.length);
+
+// showing an element of array by the index number
+console.log(numbers[2]);
+
+// showing the position of an element of array by this method
+console.log(numbers.indexOf(54));
