bc68278a7a7b3ae7c18be5a1e1ccf688331f40b3
akil-tajwar <akiltajwar@gmail.com>
2023-01-30 21:17:45 +0600

    ph notes

---
+++ Milestone 4/M20_V3-function_basic.js
@@ -0,0 +1,13 @@
+function kadarSoupKhao(){
+    console.log('rashed sir pora dhorsen');
+    console.log('ami pora pari nai');
+    console.log('sir amake bolsen kadar soup khaite');
+}
+kadarSoupKhao();
+
+
+function add(num1, num2){
+    var sum = num1 + num2;
+    console.log(sum);
+}
+add(5, 6);
---
+++ Milestone 4/M20_V4-function_with_return.js
@@ -0,0 +1,13 @@
+function add1(num1, num2){
+    var sum1 = num1 + num2;
+    console.log(sum1);
+}
+add1(5, 6);
+
+
+function add2(num3, num4){
+    var sum2 = num3 + num4;
+    return sum2;
+}
+var total = add2(5, 9);
+console.log(total);
