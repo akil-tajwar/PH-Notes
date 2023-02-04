107f49aa3ce0f06f01fbe8ab584c3db1c0416b0d
akil-tajwar <akiltajwar@gmail.com>
2023-02-04 23:28:49 +0600

    ph notes

--- Milestone 4/M23_V7-object_sum_cart.js
+++ Milestone 4/M23_V7-object_sum_cart.js
@@ -1,13 +1,13 @@
 let cart = [
-    {name: "bag", price: 600},
-    {name: "shirt", price: 270},
-    {name: "pant", price: 380},
-    {name: "shoe", price: 720},
+    {name: "bag", price: 60, quantity: 1},
+    {name: "shirt", price: 27, quantity: 4},
+    {name: "pant", price: 38, quantity: 3},
+    {name: "shoe", price: 72, quantity: 2},
 ]
 function totalPrice(cart){
     let total = 0;
     for(let i=0; i<cart.length; i++){
-        total = total+cart[i].price;
+        total = total+(cart[i].price*cart[i].quantity);
     }
     return total;
 }
