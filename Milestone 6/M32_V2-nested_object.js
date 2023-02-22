eeea125e3bbc9dc1b33020927ce7d478f1230fa3
akil-tajwar <akiltajwar@gmail.com>
2023-02-23 00:34:00 +0600

    ph notes

---
+++ Milestone 6/M32_V2-nested_object.js
@@ -0,0 +1,12 @@
+const sadia = [{
+    name: 'Sadia Humburger',
+    id: 87,
+    department: 'English',
+    phoneNumber: {
+        num1: '01234567890',
+        num2: '09876543210',
+    },
+    location: '23/A, Dhanmondi, Dhaka'
+}]
+
+console.log(sadia[0].phoneNumber.num2);
--- Milestone 6/test.js
+++
@@ -1,3 +0,0 @@
-const nums = [1,2,3,4,5];
-let output = nums.filter(n => n%2);
-console.log(output);
