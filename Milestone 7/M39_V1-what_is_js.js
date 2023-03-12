55d3645e518b9fefa2650a5e5382ad37b6e15a41
akil-tajwar <akiltajwar@gmail.com>
2023-03-12 23:19:48 +0600

    ph notes

---
+++ Milestone 7/M39_V1-what_is_js.js
@@ -0,0 +1,8 @@
+/*
+Ques: What is Javascript?
+Ans: Javascript is a JIT compiled, Multi paradigm, 
+Prototyped base, Dynamically typed, High abstraction type programming language.
+*/
+
+/*If we insert javascript code in chrome, chrome's v8 engine execute the code.
+with this v8 engine, javascript and node.js runs.*/
