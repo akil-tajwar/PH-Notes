8bc4d56f0c6ac87606a79f7e07877fdb73b5e36e
akil-tajwar <akiltajwar@gmail.com>
2023-03-14 03:11:50 +0600

    ph notes

--- Milestone 7/M42_V6-localStorage.js
+++ Milestone 7/M42_V6-localStorage.js
@@ -8,5 +8,12 @@ localStorage.getItem('drinks');
 // localStorage.removeItem('key');
 localStorage.removeItem('drinks');
 
+//clear all data from browser.
+localStorage.clear;
+
+//measures how many datas are stored
+localStorage.length;
+
 /*local storage always store data in string. if i store int data, 
-it will convert it into string automatically.*/
+it will convert it into string automatically. array and object won't
+work normally.*/
---
+++ Milestone 7/M42_V7-localStorage_array_and_obj.js
@@ -0,0 +1,11 @@
+/*if we want to save any object in localStorage, we have to convert the object 
+into string. for this, we have to follow this method -
+
+JSON.stringify(key)
+
+
+
+
+now, if we want to get the object from a string, we have to follow this mehtod - 
+
+JSON.parse(stringName)*/
