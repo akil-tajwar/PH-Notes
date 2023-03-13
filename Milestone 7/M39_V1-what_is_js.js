c9eca52487c99e529d684362d2ae1f9dc6393149
akil-tajwar <akiltajwar@gmail.com>
2023-03-14 00:34:40 +0600

    ph notes

--- Milestone 7/M39_V1-what_is_js.js
+++ Milestone 7/M39_V1-what_is_js.js
@@ -6,3 +6,8 @@ Prototyped base, Dynamically typed, High abstraction type programming language.
 
 /*If we insert javascript code in chrome, chrome's v8 engine execute the code.
 with this v8 engine, javascript and node.js runs.*/
+
+/*
+Ques: What is event loop in javascript?
+Ans: blog link - https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
+video link - https://www.youtube.com/watch?v=8aGhZQkoFbQ&ab_channel=JSConf*/
---
+++ Milestone 7/M42_V8-local_storage.js
@@ -0,0 +1,12 @@
+//localStorage.setItem('key', value);
+localStorage.setItem('drinks', 20);
+localStorage.setItem('rice', 'kacchi');
+
+//localStorage.getItem('key');
+localStorage.getItem('drinks');
+
+// localStorage.removeItem('key');
+localStorage.removeItem('drinks');
+
+/*local storage always store data in string. if i store int data, 
+it will convert it into string automatically.*/
