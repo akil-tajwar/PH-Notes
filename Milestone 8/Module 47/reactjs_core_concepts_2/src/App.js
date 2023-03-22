204015054281ce49d971c36aa84556701240093c
akil-tajwar <akiltajwar@gmail.com>
2023-03-22 14:46:52 +0600

    ph notes

--- Milestone 8/Module 47/reactjs_core_concepts_2/src/App.js
+++ Milestone 8/Module 47/reactjs_core_concepts_2/src/App.js
@@ -1,15 +1,44 @@
-/* useState() put the state variable in the functional component*/
+/* useState() put the state variable in the functional component In easy
+word, when we have to update a value in ui, we will use useState()*/
 
-import { useState } from 'react';
+/* we had to call the fetching function in the bottom in order to show the data
+in ui. in react, we will use useEffect() */
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* counter */}
       <Counter></Counter>
+
+      {/* external users */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+// external users
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+    </div>
+  )
+}
+
+// counter
 function Counter(){
   const [count, setCount] = useState(32);
   const increaseCount = () =>{setCount(count+1)};
