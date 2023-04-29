c0154bf484c2c5adbfbc49306200a451477c6be4
akil-tajwar <akiltajwar@gmail.com>
2023-04-29 21:31:34 +0600

    ph notes

---
+++ Milestone 10/Module 60/auth-context-tailwind/src/Components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
+
+const Header = () => {
+    const {user , logOut} = useContext(AuthContext);
+    console.log(user);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error))
+    }
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="flex-none">
+                    <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </button>
+                </div>
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none">
+                    <div className='flex gap-5'>
+                        <Link to='/'>Home</Link>
+                        {
+                            user ? <span onClick={handleLogOut} className='cursor-pointer'>Signout</span> : <Link to='/login'>Login</Link>
+                        }
+                        {
+                            user ? <span>{user.email}</span> : <Link to='/register'>Register</Link>
+                        }
+                    </div>
+                    <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ Milestone 10/Module 60/auth-context-tailwind/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8APNiehSe8c8WTwqRHVI3wcrIXxI5u4g",
+  authDomain: "auth-firebase-context-ta-b5f48.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-b5f48",
+  storageBucket: "auth-firebase-context-ta-b5f48.appspot.com",
+  messagingSenderId: "1018514274699",
+  appId: "1:1018514274699:web:a55de28cadaa3c84c7d78e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
